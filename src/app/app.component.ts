e1737e51b490d3853741f0feb6757852e8639030
Kelsey Langlois <kels.langlois@gmail.com>
2017-10-30 10:03:29 -0700

    Add categories to navbar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Category } from './category.model';
+import { CategoryService } from './category.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [CategoryService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  categories: Category[];
+
+  constructor(private categoryService: CategoryService, private router: Router) {}
+
+  ngOnInit() {
+    this.categories = this.categoryService.getCategories();
+  }
 
 }
